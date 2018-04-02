439572b3feeca2f25f2d72cf0fc79a7b0f570f3d
Itamar Gafni <itamargafni@gmail.com>
2018-04-02 12:46:21 +0300

    typed in ex15

--- ex15.py
+++ ex15.py
@@ -1,3 +1,14 @@
-# ex 15
+from sys import argv
 
-# comment
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's you file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+txt_again = open(file_again)
+
+print(txt_again.read())
