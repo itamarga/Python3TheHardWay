c9354e4a4308626555b43f28367bd32065f67320
Itamar Gafni <itamargafni@gmail.com>
2018-04-03 19:45:38 +0300

    ex23

---
+++ ex23.py
@@ -0,0 +1,24 @@
+import sys
+script, read_file, input_encoding, error = sys.argv
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+def print_line(line, encoding, errors):
+    next_lang = line#.strip()
+    # raw_bytes = line.encode(encoding, errors=errors)
+    cooked_string = next_lang.decode(encoding, errors=errors)
+    # byte_file.write(raw_bytes)
+    line = line.remove(0)
+
+    print(line, "<===>", cooked_string)
+    # print(next_lang)
+
+languages = open(read_file, encoding="utf-8")
+# languages = open(read_file, "rb")
+byte_file = open("raw_bytes.txt", "rb")
+
+main(byte_file, input_encoding, error)
