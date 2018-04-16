dde6bd59425a9660ee30869d34039eb8e70ee0d9
Itamar Gafni <itamargafni@gmail.com>
2018-04-16 14:32:25 +0300

    testing imports

---
+++ math/math.py
@@ -0,0 +1,6 @@
+def ceil(x):
+    """Return the bottom of x as a float, the smallest integet value lesser than or equal to x."""
+    return int(x)
+
+# num = 11.9
+# print(ceil(num))
---
+++ testimport.py
@@ -0,0 +1,6 @@
+import sys
+import math
+
+num = 11.9
+print(math.ceil(num))
+print(sys.path)
